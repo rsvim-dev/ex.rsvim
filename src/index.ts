3571147fd1459be021e4cc3509524bceb8072f49
linrongbin16 <linrongbin16@outlook.com>
2026-06-09 12:49:18 +0800

    c

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,6 @@ const DefaultSetupOptions: SetupOptions = {
 
 function setup(setupOptions?: SetupOptions): void {
   setupOptions = setupOptions ?? DefaultSetupOptions;
-  // setDefaultSetupOptions(setupOptions, DefaultSetupOptions);
 
   [quit, write].forEach((cmd) => {
     let options = cmd.options;

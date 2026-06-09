12805832dfee50893bffa86ffe2751a5e713f21f
linrongbin16 <linrongbin16@outlook.com>
2026-06-09 10:06:49 +0800

    c

--- src/write.ts
+++ src/write.ts
@@ -1,5 +1,5 @@
 import type { ExDefinition } from ".";
-import type { RsvimCmd } from "@rsvim/types";
+import type { RsvimCmd } from "rsvim-types";
 
 async function write(ctx: RsvimCmd.CommandContext): Promise<void> {
   const bufId = ctx.currentBufferId;
